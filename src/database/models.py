a187a6d0da1b8d0eed3dd01630080dab84378428
Jipianu Mihnea <jipianu_mihnea@yahoo.com>
2025-02-11 17:49:26 +0200

    created models

--- src/database/models.py
+++ src/database/models.py
@@ -5,14 +5,38 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = "users"
     userID = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(50), nullable=False, unique=True)
     mail = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False) # hashed password
 
     def __repr__(self):
         return f"<User {self.username}>"
     
+class File(db.Model):
+    __tablename__="files"
+    fileID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    filename = db.Column(db.String(100), nullable=False)
+    latestVersion = db.Column(db.Integer, nullable=False)
+    
+    def __repr__(self):
+        return f"<File {self.filename}>"
 
+class UserFile(db.Model):
+    __tablename__="userfile"
+    userID = db.Column(db.Integer, db.ForeignKey("users.userID"), primary_key=True)
+    fileID = db.Column(db.Integer, db.ForeignKey("files.fileID"), primary_key=True)
+    accessMode = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<UserFile User:{self.userID} File:{self.fileID} AccessMode:{self.accessMode}>"
+
+class Version(db.Model):
+    __tablename__ = "versions"
+    fileID = db.Column(db.Integer, db.ForeignKey("files.fileID"), primary_key=True)  # Foreign key to Files
+    versionNumber = db.Column(db.Integer, primary_key=True)  # Version number acts as part of primary key
+    fileCID = db.Column(db.String(255), nullable=False)  # IPFS CID
+
+    def __repr__(self):
+        return f"<Version File:{self.fileID} Version:{self.versionNumber}>"
 
 def init_db(app):
     db.init_app(app)
